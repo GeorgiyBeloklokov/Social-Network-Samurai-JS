c77d34e16887833927eafe78f25ed25ccf30425f
George <George@gmail.com>
2021-04-01 10:44:06 +0300

    change

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 
 
 const MyPosts = () => {
     return <div>
+        <div>
+            < img className={s.image} src='https://universalnews.org/wp-content/uploads/2020/05/Facebook-Avatar.jpg' alt = " "/>
+
+        </div>
+        <div>
+            <textarea placeholder={"YOUR TEXT"}> </textarea>
+<button>Add</button>
+        </div>
             <div className={s.ithem} >my post</div>
-            <div className={s.ithem} >new post</div>
-            <div className={s.ithem} >post 1</div>
-            <div className={s.ithem} >post 2</div>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+
         </div>
 
 
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import s from "./Post.module.css";
+import d from "./Post.module.css";
 
 
 const Post = () => {
     return <div>
-            <div className={s.ithem} >my post</div>
+        <div>
+            < img className = {d.image} src ='https://bipbap.ru/wp-content/uploads/2017/08/0447e02caa3a98459dcc5f7b5e8051cd.jpg' alt=" "/>
+    </div>
+        <div className= {d.ithem} >Post</div>
 
         </div>
-
-
-
 }
 export default Post;

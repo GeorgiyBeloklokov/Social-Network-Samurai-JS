4432b2252dbdec6553bae6ca0e011a4597f5c33a
George <George@gmail.com>
2021-04-08 15:11:42 +0300

    refactoring

--- src/components/Dialogs/DialogIthem/DialogIthem.jsx
+++ src/components/Dialogs/DialogIthem/DialogIthem.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import f from "./../Dialogs.module.css";
 import {NavLink} from "react-router-dom";
-import d from "../../Profile/MyPosts/Post/Post.module.css";
 
 const DialogIthem = (props) => {
     let path = "/dialogIthem/" + props.id;
-    let avatarMessages = (props) => {
-        return (
+    return (
         <div>
-            < img className={d.image}
-                  src={props.url} alt=" "/>
+            <div className={f.dialog}>
+                <NavLink to={path}>{props.name} </NavLink>
+            </div>
+            <div>
+                < img className={f.image}
+                      src={props.url} alt=" "/>
+            </div>
         </div>
-        )
-    }
-    return <div className={f.dialog}>
-        <NavLink to={path}>{props.name + avatarMessages } </NavLink>
-    </div>
+    )
 }
-
-
 export default DialogIthem;

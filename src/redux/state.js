d767e714686cc41102e2c69274e0fc069a148fb6
TartasM <georgiy.reus@gmail.com>
2021-04-21 13:27:56 +0300

    refacoring, action creator in business and refactoring

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,8 @@
 
+const ADD_POST = 'ADD--POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+
 let store = {
     _state: {
         profilePage: {
@@ -97,7 +101,7 @@ let store = {
 
     dispatch(action) {
         //Post area
-        if (action.type === 'ADD--POST') {
+        if (action.type === ADD_POST ) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -106,7 +110,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = ' ';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
@@ -139,16 +143,8 @@ let store = {
     }
 }
 
- export const addPostActionCreator = () => {
-    return {
-        type: 'ADD--POST'
-    }
-}
-export const updateNewPostTextActionCreator = (text) => {
-    return {
-        type: 'UPDATE-NEW-POST-TEXT', newText: text
-    }
-}
+ export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
 
 

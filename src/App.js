8d6b43472d62aa532eb30872762135a0c56fd9fd
TartasM <georgiy.reus@gmail.com>
2021-04-27 10:39:02 +0300

    container for Dialogs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
 import {Route} from "react-router-dom";
 import News from "./components/News/News";
